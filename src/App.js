a791f2073fb63185bac5d0584ec4296682b468af
danr0x <danilo@gritlab.ax>
2023-03-08 21:26:49 +0200

    intro text with effect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
+function LoadIntroText(){
+  return (
+    <>
+    I am a text that contains a short description
+    </>
+  )
+}
+
+const SomeComponent = ({ item }) => {
+  const duration = 200; //ms
+  const delay = 600; //ms
+  const animStr = () =>`fadeIn ${duration}ms ease-out ${delay * (Math.random())}ms forwards`;
+  let characters = item.split('')
+  console.log(characters)
+  return (
+  <>
+  <div style={{ display: "block", position: "relative" }}>
+  {characters.map(char => <div className="animate" style={{display: "inline-block", animation: animStr()}}>{char}</div> )}
+  </div>
+  </>
+  )
+}
+
 function App() {
+  const text = "Hello! I am Danilo Canguçu, an aspiring front-end developer."
+  let textSplit = text.split(' ')
+  console.log(Math.random())
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>
+            {textSplit.map((word, i) =>
+              <SomeComponent key={i} item={word}/>
+            )}
+          </div>
       </header>
     </div>
   );
