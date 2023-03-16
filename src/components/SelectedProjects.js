1d7b9abb427015eabc9c043363ca4d654506b8d8
danr0x <danilo@gritlab.ax>
2023-03-16 15:15:11 +0200

    project links and description

--- src/components/SelectedProjects.js
+++ src/components/SelectedProjects.js
@@ -12,35 +12,41 @@ const Projects = () => (
     <div className="projects">
         <Project
         name="Pacman"
+        description="Duo project for desktop."
         tech="JavaScript, HTML and CSS"
-        role="JavaScript logics and DOM manipulation, CSS and HTML"
+        role="part of JavaScript logics, CSS and HTML"
         video="https://github.com/danilocangucu/my-website/blob/main/src/components/pacman.mp4?raw=true"
-        github="https://github.com/danilocangucu/pacman"/>
+        github="https://github.com/danilocangucu/pacman-js/tree/master"
+        link="https://tranquil-tarsier-fe7b59.netlify.app/"/>
         <Project
-        name="Pacman"
-        tech="JavaScript, HTML and CSS"
-        role="JavaScript logics and DOM manipulation, CSS and HTML"
+        name="My website"
+        description="The website you are now!"
+        tech="React.js, HTML and CSS"
+        role="building the project from scratch."
         video="https://github.com/danilocangucu/my-website/blob/main/src/components/pacman.mp4?raw=true"
-        github="https://github.com/danilocangucu/pacman"/>
+        github="https://github.com/danilocangucu/my-website"
+        link="https://tranquil-tarsier-fe7b59.netlify.app/"/>
     </div>
-    <div className="projects">
+    {/* <div className="projects">
         <Project
         name="Pacman"
         tech="JavaScript, HTML and CSS"
         role="JavaScript logics and DOM manipulation, CSS and HTML"
         video="https://github.com/danilocangucu/my-website/blob/main/src/components/pacman.mp4?raw=true"
-        github="https://github.com/danilocangucu/pacman"/>
+        github="https://github.com/danilocangucu/pacman"
+        link="https://tranquil-tarsier-fe7b59.netlify.app/"/>
         <Project
         name="Pacman"
         tech="JavaScript, HTML and CSS"
         role="JavaScript logics and DOM manipulation, CSS and HTML"
         video="https://github.com/danilocangucu/my-website/blob/main/src/components/pacman.mp4?raw=true"
-        github="https://github.com/danilocangucu/pacman"/>
-    </div>
+        github="https://github.com/danilocangucu/pacman"
+        link="https://tranquil-tarsier-fe7b59.netlify.app/"/>
+    </div> */}
     </>
 )
 
-const Project = ({ name, tech, role, video, github }) => {
+const Project = ({ name, description, tech, role, video, github, link }) => {
     return (
         <div className="project">
             <div className="project-video-container">
@@ -51,14 +57,14 @@ const Project = ({ name, tech, role, video, github }) => {
             </div>
             <div className="project-text">
                 <h2>{name}</h2>
-                Duo project for desktop.
+                {description}
                 <br/>Made with {tech}.
                 I was responsible for {role}.
                 <br/><br/>
                     <a href={github}>
                     <img
                     src="https://github.com/danilocangucu/my-website/raw/main/src/components/github-mark-white.png"
-                    height="13" alt="GitHub logo"></img> Repository</a> <a href="http://">View</a>  
+                    height="13" alt="GitHub logo"></img> Repository</a> <a href={link}>View</a>  
             </div>
         </div>
     )
