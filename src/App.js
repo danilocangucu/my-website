37ff8b5ac904bbc6ec67a823732c3d13238d63df
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-14 13:46:12 +0200

    refactored App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
-import Intro from './components/Intro'
-import Bio from './components/Bio'
-import SelectedProjects from './components/SelectedProjects'
-import Skills from './components/Skills'
-import Footer from './components/Footer'
-
-const animStr = () =>`fadeIn 1000ms ease-out ${1000 * (Math.random())}ms forwards`;
-
-const ArrowDown = () => {
-  const arrowDown = document.getElementsByClassName('arrow-down')
-  setTimeout(() => {
-    arrowDown[0].style.animation = animStr()
-  }, 1500)
-  return <div className='arrow-down'>↓</div>
-
-}
+import AppRoutes from "./AppRoutes";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Intro />
-      <ArrowDown />
-      <Bio />
-      <SelectedProjects />
-      <Skills />
+      <AppRoutes />
       <Footer />
     </div>
   );
