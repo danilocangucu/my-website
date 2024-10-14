9145528a3361f7fdfcb59c95b8c92725e95ac7a4
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-14 13:58:50 +0200

    added a draft for NotFound component

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { useRoutes, BrowserRouter as Router } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loading from "./components/Loading";
+import NotFound from "./components/NotFound";
 
 const Home = lazy(() => import("./components/Home"));
 const OfflinePage = lazy(() => import("./components/OfflinePage"));
@@ -9,6 +10,7 @@ const Routes = () =>
   useRoutes([
     { path: "/", element: <Home /> },
     { path: "/offline", element: <OfflinePage /> },
+    { path: "*", element: <NotFound /> },
   ]);
 
 export default function AppRoutes() {
