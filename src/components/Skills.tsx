6b97bc072cca1a34637b638a7a69c76fd61f03bb
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-24 00:23:10 +0200

    refactored Skills components with imports

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,110 +1,16 @@
 import React from "react";
-
-import { arrangeSkillsByCategory, formatCategoryName, glowShadow, logosBaseUrl, populateMarquee, Skill } from "../utils/SkillsUtils";
 import Marquee from "react-fast-marquee";
 
-// TODO add missing skills
-// TODO implement the backend for skills and fetch them here
-const skillsData = [
-    {
-        name: "React",
-        logoFile: `react.png`,
-        skillUrl: "https://reactjs.org/",
-        category: "Frontend",
-        useLightGlow: false,
-    },
-    {
-        name: "Redux",
-        logoFile: `redux.png`,
-        skillUrl: "https://redux.js.org/",
-        category: "Frontend",
-        useLightGlow: true,
-    },
-    {
-        name: "Python",
-        logoFile: `python.png`,
-        skillUrl: "https://www.python.org/",
-        category: "Backend",
-        useLightGlow: false,
-    },
-    {
-        name: "Java",
-        logoFile: `java.png`,
-        skillUrl: "https://www.oracle.com/java/",
-        category: "Backend",
-        useLightGlow: false,
-    },
-    {
-        name: "Spring",
-        logoFile: `spring.png`,
-        skillUrl: "https://spring.io/",
-        category: "Backend",
-        useLightGlow: false,
-    },
-    {
-        name: "GraphQL",
-        logoFile: `graphql.png`,
-        skillUrl: "https://graphql.org/",
-        category: "Databases",
-        useLightGlow: true,
-    },
-    {
-        name: "AWS",
-        logoFile: `aws.png`,
-        skillUrl: "https://aws.amazon.com/",
-        category: "cloud_and_dev_ops",
-        useLightGlow: true,
-    },
-    {
-        name: "Linux",
-        logoFile: `linux.png`,
-        skillUrl: "https://www.linux.org/",
-        category: "Other",
-        useLightGlow: true,
-    },
-    {
-        name: "Ubuntu",
-        logoFile: `ubuntu.png`,
-        skillUrl: "https://www.linux.org/",
-        category: "Other",
-        useLightGlow: true,
-    },
-    {
-        name: "Photoshop",
-        logoFile: `photoshop.png`,
-        skillUrl: "https://www.adobe.com/products/photoshop.html",
-        category: "Other",
-        useLightGlow: true,
-    },
-    {
-        name: "Jest",
-        logoFile: `jest.png`,
-        skillUrl: "https://jestjs.io/",
-        category: "Testing",
-        useLightGlow: true,
-    },
-    {
-        name: "Azure",
-        logoFile: `azure.png`,
-        skillUrl: "https://azure.microsoft.com/",
-        category: "familiar_with",
-        useLightGlow: false,
-    },
-    {
-        name: "Google Cloud Console",
-        logoFile: `google_cloud_console.png`,
-        skillUrl: "https://cloud.google.com/console/",
-        category: "familiar_with",
-        useLightGlow: false,
-    },
-    {
-        name: "Kotlin",
-        logoFile: `kotlin.png`,
-        skillUrl: "https://kotlinlang.org/",
-        category: "familiar_with",
-        useLightGlow: true,
-    },
-];
+import {
+    arrangeSkillsByCategory,
+    formatCategoryName,
+    glowShadow,
+    logosBaseUrl,
+    populateMarquee,
+    Skill,
+    // TODO implement the backend for skillsData and fetch them here
+    skillsData
+} from "../utils/SkillsUtils";
 
 export const LogosMarquee: React.FC<{ skillsLogos: Skill[] }> = ({ skillsLogos }) => {
     const populatedMarquee = populateMarquee(skillsLogos, 20);
@@ -147,9 +53,11 @@ const Skills: React.FC = () => {
     const mappedSkills = mapSkillsToCategories(skills);
 
     return (
+        // TODO option to change view from marquee to grid
         <div className="skills-body">
             <h1>{h1}</h1>
             {mappedSkills}
+            { /* TODO fix this <span> */}
             <span>&nbsp;</span>
         </div>
     );
