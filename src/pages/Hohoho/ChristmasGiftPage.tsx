3f3df93e6b69c312326a47547ed36a22694d63d4
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-05 15:46:34 +0100

    wip: ChirstmasGiftPage component

---
+++ src/pages/Hohoho/ChristmasGiftPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ChristmasGiftPage = () => {
+  return (
+    <div className="christmas-body">
+      <div className='u-container'>
+        <header>
+          <h1 className='mountains-of-christmas-bold mountains-o-c-b--h1'>Ho! Ho! Ho!</h1>
+          <h2 className="merriweather-regular merriweather-r--h2">Get Ready for a Christmas Gift You’ll Never Forget!</h2>
+        </header>
+        <section className="p-grid">
+          <p className="quattrocento-regular">
+            This year, I’m giving you something special: a free, custom-made website! Imagine a beautiful site to showcase your ideas, hobbies, or business—ready to go for the new year. It’s a gift designed just for you. Everyone deserves some extra sparkle this season!
+          </p>
+          <p className="quattrocento-regular">
+            To have a chance to win this fantastic Christmas gift, simply fill out the form below. It only takes a few minutes, and you could be on your way to an incredible new online presence. Just tell us a bit about your vision, and we’ll take care of the rest. Don't miss this opportunity for a holiday surprise that keeps on giving long after the season is over. Merry Christmas, and good luck!
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default ChristmasGiftPage;
