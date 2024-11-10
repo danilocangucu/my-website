5c4b54d50b2f551d488f9ec3cc9b8950de707a9f
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-10 19:11:50 +0100

    placed animations to its new folder

--- src/pages/Hohoho/Animations/SnowAnimation.tsx
+++ src/pages/Hohoho/Animations/SnowAnimation.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-import { useResizeHandler } from "../../hooks/useResizeHandler";
-import { useSnowFlakeAnimation } from "../../hooks/useSnowFlakesAnimation";
-import { calculateSnowflakeAmount, createAllSnowflakeDivs } from "../../utils/HohohoUtils";
+import { useResizeHandler } from "../../../hooks/useResizeHandler";
+import { useSnowFlakeAnimation } from "../../../hooks/useSnowFlakesAnimation";
+import { calculateSnowflakeAmount, createAllSnowflakeDivs } from "../../../utils/HohohoUtils";
 
 const SnowAnimation: React.FC = () => {
     const { currentHeight, currentWidth } = useResizeHandler(window.innerHeight, window.innerWidth);
     const snowflakesAmount = calculateSnowflakeAmount(currentHeight, currentWidth);
 
     // TODO snowflakes should be svgs, now they are pngs
-    // TODO styles of margin between snow flakes should also be dinamically set
     const createdSnowflakesDivs = createAllSnowflakeDivs(snowflakesAmount);
 
     // TODO useSnowFlakeAnimation should also be called with currentWidth
