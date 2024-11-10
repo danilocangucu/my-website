3cda0b2226c921da574e5e728dd228b71ff2bda6
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-10 19:18:57 +0100

    updated Hohoho component with new components

--- src/pages/Hohoho/Hohoho.tsx
+++ src/pages/Hohoho/Hohoho.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-import SnowAnimation from './SnowAnimation'
-import ChristmasGiftPage from './ChristmasGiftPage'
-import Separator from './Separator'
-import HohohoAnimation from './HohohoAnimation'
+import SnowAnimation from './Animations/SnowAnimation'
+import HohohoAnimation from './Animations/HohohoAnimation'
+import Intro from './Intro'
+import Deadlines from './Deadlines'
+import Criteria from './Criteria/Criteria'
+import CallToAction from './CallToAction'
+import Breadcrumbs from './BreadCrumbs'
+import Footer from './Footer'
 
 function Hohoho() {
     return (
@@ -11,8 +15,12 @@ function Hohoho() {
             <SnowAnimation />
             <div className='overlay text-white'>
                 <HohohoAnimation />
-                <ChristmasGiftPage />
-                <Separator separator="lights" />
+                <Breadcrumbs separator='❄' />
+                <Intro separator="branch1" />
+                <Deadlines separator="lights" />
+                <Criteria separator="branch2" />
+                <CallToAction />
+                <Footer />
             </div>
         </div>
     )
