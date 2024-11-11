04ecfd484cdb867945d855f7fd1d1f2ca7453662
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-11 14:37:43 +0100

    set opacity 0 to Separator component

--- src/pages/Hohoho/Separator.tsx
+++ src/pages/Hohoho/Separator.tsx
@@ -5,7 +5,7 @@ const Separator = ({ separator, separatorRef }: { separator: string, separatorRe
 
     // TODO separator is not resized properly in small screens, height should go as smaller as possible
     return (
-        <div className='img-grid' ref={separatorRef}>
+        <div className='img-grid' ref={separatorRef} style={{ opacity: 0 }}>
             <img
                 src={imageSrc}
                 alt="separator"
