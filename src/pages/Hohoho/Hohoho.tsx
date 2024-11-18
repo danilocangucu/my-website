438bb545d8964e58e712fa85e09f4fe39d664656
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-18 20:45:00 +0100

    updated Hohoho using multiple languages

--- src/pages/Hohoho/Hohoho.tsx
+++ src/pages/Hohoho/Hohoho.tsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
+import i18next from 'i18next'
 
-import SnowAnimation from './Animations/SnowAnimation'
-import HohohoAnimation from './Animations/HohohoAnimation'
-import Intro from './Intro'
-import Deadlines from './Deadlines'
-import Criteria from './Criteria/Criteria'
-import CallToAction from './CallToAction'
-import Breadcrumbs from './BreadCrumbs'
-import Footer from './Footer'
+import SnowAnimation from './components-animations/SnowAnimation'
+
+// Translations
+// Shared
+import enShared from './translations/shared/en.json'
+import esShared from './translations/shared/es.json'
+import ptbrShared from './translations/shared/ptbr.json'
+// Landing Page
+import enLandingPage from './translations/landing-page/en.json'
+import esLandingPage from './translations/landing-page/es.json'
+import ptbrLandingPage from './translations/landing-page/ptbr.json'
+// Application Page
+import enApplicationPage from './translations/application-page/en.json'
+import esApplicationPage from './translations/application-page/es.json'
+import ptbrApplicationPage from './translations/application-page/ptbr.json'
+
+import useLanguageDetection from './hooks/useLanguageDetection'
+import useLanguage from './hooks/useLanguage'
+
 
 function Hohoho() {
+    // TODO debug language detection
+    useLanguageDetection();
+    useLanguage();
+
+    // TODO refactor Hohoho useEffect
+    useEffect(() => {
+        // Translations
+        // Shared
+        i18next.addResources('en', 'hohoho/shared', enShared);
+        i18next.addResources('es', 'hohoho/shared', esShared);
+        i18next.addResources('ptbr', 'hohoho/shared', ptbrShared);
+        // Landing Page
+        i18next.addResources('en', 'hohoho/landing-page', enLandingPage);
+        i18next.addResources('es', 'hohoho/landing-page', esLandingPage);
+        i18next.addResources('ptbr', 'hohoho/landing-page', ptbrLandingPage);
+        // Application Page
+        i18next.addResources('en', 'hohoho/application-page', enApplicationPage);
+        i18next.addResources('es', 'hohoho/application-page', esApplicationPage);
+        i18next.addResources('ptbr', 'hohoho/application-page', ptbrApplicationPage)
+    }, []);
+
     return (
         <div className='hohoho-component-container'>
             <SnowAnimation />
             <div className='overlay text-white'>
-                <HohohoAnimation />
-                <Breadcrumbs separator='∙' />
-                <Intro separator="branch1" />
-                <Deadlines separator="lights" />
-                <Criteria separator="branch2" />
-                <CallToAction />
-                <Footer />
+                <div className="hohoho-page-container">
+                    <Outlet />
+                </div>
             </div>
         </div>
     )
