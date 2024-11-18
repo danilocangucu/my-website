042ffd6b00c5aa1bdc729e5ef74e6bb56f56e0e5
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-18 20:44:26 +0100

    updated AppRoutes for hohoho project

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,9 +3,11 @@ import { useRoutes, BrowserRouter as Router } from "react-router-dom";
 
 import Loading from "./components/Loading";
 import NotFound from "./components/NotFound";
+
 import Hohoho from "./pages/Hohoho/Hohoho";
-import LandingPage from "./pages/Hohoho/LandingPage";
-import ApplicationPage from "./pages/Hohoho/ApplicationPage";
+
+import LandingPage from "./pages/Hohoho/pages/LandingPage/LandingPage";
+import ApplicationPage from "./pages/Hohoho/pages/ApplicationPage/ApplicationPage";
 
 const Home = lazy(() => import("./components/Home"));
 const StatusPage = lazy(() => import("./pages/StatusPage/StatusPage"));
