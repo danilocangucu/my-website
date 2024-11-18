6e906b4307312c93924e8ed188b42f5bb9ba8e49
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-18 20:43:45 +0100

    added i18n in index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import './index.css';
 import App from './App';
+import "./i18n/i18n";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
