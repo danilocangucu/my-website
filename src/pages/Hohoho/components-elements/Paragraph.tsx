ad0ab9c4231e928a1ba6a2d978655bf5acbe5134
Danilo Canguçu <daniloccangucu@gmail.com>
2024-12-23 21:23:07 +0100

    Paragraph with CustomLink component

--- src/pages/Hohoho/components-elements/Paragraph.tsx
+++ src/pages/Hohoho/components-elements/Paragraph.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import { extractLinksFromText } from '../utils/ParagraphUtils';
+import CustomLink from '../components-shared/CustomLink';
+
 interface ParagraphProps {
     children: React.ReactNode;
     additionalClassNames?: string[];
 }
 
 const Paragraph: React.FC<ParagraphProps> = ({ children, additionalClassNames = [] }) => {
+    const processedContent = extractLinksFromText(children as string) as { type: string; linkText: string; linkUrl: string; key: number; content?: undefined; }[] | { type: string; content: string; key: number; linkText?: undefined; linkUrl?: undefined; }[];
+
     return (
-        <p
-            className={classNames('quattrocento-regular', ...additionalClassNames)}
-        >
-            {children as string}
+        <p className={classNames('quattrocento-regular', ...additionalClassNames)}>
+            {Array.isArray(processedContent) && processedContent.map((part) => {
+                if (part.type === 'link') {
+                    return (
+                        <CustomLink key={part.key} url={part.linkUrl!}>
+                            {part.linkText}
+                        </CustomLink>
+                    );
+                }
+                return part.content;
+            })}
         </p>
     );
 };
